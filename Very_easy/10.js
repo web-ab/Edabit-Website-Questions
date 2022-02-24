36443fee735121585160a4227cebc75dedfb705b
web-ab <abolfazlsadeghiweb@gmail.com>
2022-02-24 14:57:40 -0800

    Very Easy Level

--- Very_easy/10.js
+++ Very_easy/10.js
@@ -32,4 +32,4 @@ console.log(nextEdge(5, 4)); // 8
 console.log(nextEdge(8, 3)); // 10
 console.log(nextEdge(7, 9)); // 15
 console.log(nextEdge(10, 4)); // 13
-console.log(nextEdge(7, 2)); // 8
+console.log(nextEdge(7, 2)); // 8 
