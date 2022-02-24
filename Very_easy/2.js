a6a1b293b558a70edf208b62e2980f923860e867
web-ab <abolfazlsadeghiweb@gmail.com>
2022-02-24 13:27:49 -0800

    Very Easy Level

--- Very_easy/2.js
+++ Very_easy/2.js
@@ -16,8 +16,6 @@
 /**
  * Notes :
  . Don't forget to return the result.
- . If you get stuck on a challenge, find help in the Resources tab.
- . If you're really stuck, unlock solutions in the Solutions tab.
  */
 
 
---
+++ Very_easy/3.js
@@ -0,0 +1,32 @@
+// 2. Third question
+
+
+// Question : Return the Next Number from the Integer Passed
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+
+
+// Examples
+
+// . addition(0) ➞ 1
+// . addition(9) ➞ 10
+// . addition(-3) ➞ -2
+
+
+/**
+ * Notes :
+ . Don't forget to return the result.
+ */
+
+
+// Answer
+
+function addition(num) {
+    return num + 1;
+}
+
+console.log(addition(2)); // 2 plus 1 equals 3.
+console.log(addition(-9));// -8 plus 1 equals -9.
+console.log(addition(0)); // 0 plus 1 equals 1.
+console.log(addition(999)); // 999 plus 1 equals 1000.
+console.log(addition(73)); // 73 plus 1 equals 74.
