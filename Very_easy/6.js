41ef0ae78128f56e7d527b133b7807c3ca771ec0
web-ab <abolfazlsadeghiweb@gmail.com>
2022-02-24 13:59:18 -0800

    Very Easy Level

---
+++ Very_easy/6.js
@@ -0,0 +1,35 @@
+// 6. Six question
+
+
+// Question : Buggy Code (Part 1)
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+
+
+
+// Examples
+
+// . cubes(3) ➞ 27
+// . cubes(5) ➞ 125
+// . cubes(10) ➞ 1000
+
+
+/**
+ * Notes :
+ . READ EVERY WORD CAREFULLY, CHARACTER BY CHARACTER!
+ . Don't overthink this challenge; it's not supposed to be hard.
+ */
+
+
+// Answer
+
+
+function cubes(a) {
+	return a ** 3
+}
+
+console.log(cubes(2)); // 8
+console.log(cubes(3)); // 27
+console.log(cubes(4)); // 64
+console.log(cubes(5)); // 125
+console.log(cubes(10)); // 1000
