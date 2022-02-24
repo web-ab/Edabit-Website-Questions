4114a56888765fe93133333435154e22329ac653
web-ab <abolfazlsadeghiweb@gmail.com>
2022-02-24 13:04:15 -0800

    Very Easy Level

---
+++ Very_easy/1.js
@@ -0,0 +1,34 @@
+// 1. First question
+
+
+// Question : Return the Sum of Two Numbers
+// Create a function that takes two numbers as arguments and returns their sum.
+
+
+
+// Examples
+
+// . addition(3, 2) ➞ 5
+// . addition(-3, -6) ➞ -9 
+// . addition(7, 3) ➞ 10
+
+
+/**
+ * Notes :
+ . Don't forget to return the result.
+ . If you get stuck on a challenge, find help in the Resources tab.
+ . If you're really stuck, unlock solutions in the Solutions tab.
+ */
+
+
+
+// Answer
+
+function addition(a, b) {
+    return a + b;
+}
+
+console.log(addition(2, 3)) //5
+console.log(addition(-3, -6)) // -9
+console.log(addition(7, 3)) //10
+console.log(addition(88, 2)) //90
---
+++ Very_easy/2.js
@@ -0,0 +1,40 @@
+// 2. Second question
+
+
+// Question : Convert Minutes into Seconds
+// Write a function that takes an integer minutes and converts it to seconds.
+
+
+
+// Examples
+
+// . convert(5) ➞ 300
+// . convert(3) ➞ 180
+// . convert(2) ➞ 120
+
+
+/**
+ * Notes :
+ . Don't forget to return the result.
+ . If you get stuck on a challenge, find help in the Resources tab.
+ . If you're really stuck, unlock solutions in the Solutions tab.
+ */
+
+
+// Answer
+
+
+function convert(minutes) {
+
+    // To convert minute to second we have to multiply
+    // the minute by 60 seconds because every 1 minute
+    // equals 60 seconds
+
+    return minutes * 60; // 1 minute = 60 sec
+}
+
+
+console.log(convert(6)) //360
+console.log(convert(4)) //240
+console.log(convert(8)) //480
+console.log(convert(60)) //3600
