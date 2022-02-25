b079113a82b21dc978a9343336ae7bde2697c5ec
web-ab <abolfazlsadeghiweb@gmail.com>
2022-02-25 01:01:14 -0800

    Very Easy Level

---
+++ Very_easy/14.js
@@ -0,0 +1,33 @@
+// 14. Question Fourteen
+
+
+// Question : Correct the Mistakes
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+
+
+// Examples
+
+// . squared(5) ➞ 25
+// . squared(9) ➞ 81
+// . squared(100) ➞ 10000
+
+
+/**
+ * Notes :
+ . Don't overthink this challenge; it's not supposed to be hard.
+ */
+
+
+
+// Answer
+
+function squared(b) {
+	return b * b;
+}
+
+console.log(squared(10)); // 100
+console.log(squared(69)); // 4761
+console.log(squared(666)); // 443556
+console.log(squared(-21)); // 441
+console.log(squared(21)); // 441
