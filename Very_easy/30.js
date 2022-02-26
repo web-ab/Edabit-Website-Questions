e2e8c6c4cad8ffe2061410ce7ad4811dc4a18755
web-ab <abolfazlsadeghiweb@gmail.com>
2022-02-26 10:07:21 -0800

    Very Easy Level

---
+++ Very_easy/30.js
@@ -0,0 +1,29 @@
+// Question : Boolean to String Conversion
+// Create a function that takes a boolean variable flag and returns it as a string.
+
+
+
+// Examples
+
+// . boolToString(true) ➞ "true"
+// . boolToString(false) ➞ "false"
+
+
+
+/**
+ * Notes :
+ . A profitable gamble is a game that yields a positive net profit, where net profit is calculated in the following manner: net_outcome = probability_of_winning * prize - cost_of_playing.
+ */
+
+
+
+// Answer
+
+function boolToString(flag) {
+	return flag + "";
+
+    //or falg.toString();
+}
+
+console.log(boolToString(true)); // "true"
+console.log(boolToString(false)); // false"
